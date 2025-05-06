b5587581c9e2d925545a3bd320f1843c5f991581
Aditya Pant <pantanupam05@gmail.com>
2025-05-06 23:44:55 +0530

    feat: basic connection established

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ const wss = new WebSocketServer({ port: 8081 });
 let userCount = 0;
 
 wss.on("connection", (socket) => {
-	console.log("user connected" + userCount);
 	userCount = userCount + 1;
+	console.log("user connected#" + userCount);
 });
